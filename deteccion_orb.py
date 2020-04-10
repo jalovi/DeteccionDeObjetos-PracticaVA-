f104b54f38f3609a51e96b241122dbea4676d32e
Oscarli1797 <oscarli1797@gmail.com>
2020-04-10 23:14:56 +0200

    Ejercicio 1 Detección de coches mediante puntos de interés

--- deteccion_orb.py
+++ deteccion_orb.py
@@ -2,143 +2,116 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+def lecturaImg(path_name):
+    vector_votacion=[]
+    flann=createBase(vector_votacion)
+    for img in os.listdir(path_name):
+        #Leer todas las imagenes de la carpeta test
+        imgRead = cv2.imread(path_name+"/"+img,0)
+        detectImage(imgRead,flann,vector_votacion)
+        cv2.imshow("Carimg",imgRead)
+        key = cv2.waitKey()
+        #la ventana del imagen se cierra hasta que llega un ESC
+        if key == 27:
+            cv2.destroyAllWindows()
+
+def tabla_votacion(imgread,goodKeyTrain,goodKey):
+    tamy=int((imgread.shape[0])/10)
+    tamx=int((imgread.shape[1])/10)
+    tabla = np.zeros((tamy,tamx))
+    for index in range(len(goodKey)):
+        scale=goodKeyTrain[index][4]/goodKey[index].size
+        modulo,angulo=goodKeyTrain[index][1]
+        angulo=angulo+goodKeyTrain[index][5]-goodKey[index].angle
+        modulo=modulo*scale
+        angX=np.cos(angulo)
+        angY=np.sin(angulo)
+        x0=angX*modulo
+        y0=angY*modulo
+        ejex=int((goodKey[index].pt[0]+x0)/10)
+        ejey=int((goodKey[index].pt[1]+y0)/10)
+        ejex2=int((goodKey[index].pt[0]-x0)/10)
+        ejey2=int((goodKey[index].pt[1]-y0)/10)
+        if (ejex > 0 and ejey > 0) and (ejex < tamx and ejey < tamy):
+            tabla[ejey, ejex] += 1
+        if (ejex2 > 0 and ejey2 > 0) and (ejex2 < tamx and ejey2 < tamy):
+            tabla[ejey2, ejex2] += 1
+    m=np.where(tabla==np.max(tabla))
+    return m
+
+def detectImage(imgRead,flann,vector_votacion):
+    orb = cv2.ORB_create(100, nlevels=4, firstLevel=0, scaleFactor=1.3)
+    kp,des=orb.detectAndCompute(imgRead,None)
+    matches = flann.knnMatch(des, k=5)
+    good = []
+    for matches in matches:
+        for i in matches:
+            good.append(i)
+    goodKeyTrain=acomularKey(good,vector_votacion)
+    goodKey=getKey(good,kp)
+    index=tabla_votacion(imgRead,goodKeyTrain,goodKey)
+    ejex=index[1]
+    ejey=index[0]
+    for i in range(len(ejex)):
+        x=ejex[i]*10
+        y=ejey[i]*10
+        cv2.circle(imgRead,(x,y),50,(0,255,0),2)
 
 
-def lecturaImg():
-    kpsTrain_ = []
-    dessTrain_ = []
-    kpsTest_ = []
-    dessTest_ = []
-    test = True
+def acomularKey(good,vector_votacion):
+    lista_aco=[]
+    for data in good:
+        lista_aco.append(vector_votacion[data.imgIdx][data.trainIdx])
+    return lista_aco
 
-    # Para ello creamos un FlannBasedMatcher utilizando la distancia de Hamming
-    #tenemos que crear solo uno, no uno por imagen
+def getKey(good,key):
+    lista_aco=[]
+    for data in good:
+        lista_aco.append(key[data.queryIdx])
+    return lista_aco
+
+def createBase(vector_votacion):
     FLANN_INDEX_LSH = 6
     index_params = dict(algorithm=FLANN_INDEX_LSH, table_number=6, key_size=3, multi_probe_level=1)
     search_params = dict(checks=-1)  # Maximum leafs to visit when searching for neighbours.
     flann = cv2.FlannBasedMatcher(index_params, search_params)
+    for img in os.listdir("train"):
+        imgRead = cv2.imread("train/"+img, 0)
+        orb = cv2.ORB_create(100, nlevels=4, firstLevel=0, scaleFactor=1.3)
+        kp,des=orb.detectAndCompute(imgRead,None)
+        flann.add([des])
+        vector=kps(kp,imgRead)
+        vector_votacion.append(vector)
+    return flann
 
-    os.chdir('train')
-    for img in os.listdir('.'):
-        i = img.split('.')
-        if(i[1] == 'jpg'):
-            imgRead = cv2.imread(img, 0)
-
-            kpsTrain_, des_ = orb(imgRead, kpsTrain_)
-            dessTrain_.append(des_)
-            flann.add([np.uint8(des_)])
-
-    #prueba para comparar y hacer el vector de votacion
-    os.chdir('..')
-    os.chdir('test')
-    print(os.getcwd())
-    for img in os.listdir('.'):
-        i = img.split('.')
-        if (i[1] == 'jpg'):
-            imgReadTest = cv2.imread(img, 0)
-
-            kpsTest_, des_ = orb(imgReadTest, kpsTest_)
-            dessTest_.append(des_)
-
-            tabla = np.zeros((imgReadTest.shape[0], imgReadTest.shape[1]))
-            tablaVotacion(tabla, kpsTrain_, kpsTest_, des_, flann, imgReadTest, img)
-
-            #centro = compararMatcher(imgRead, imgReadTest, kpsTrain_, kpsTest_)
-
-def orb(img,kps_):
-    # inicializamos ORB
-    orb = cv2.ORB_create(100, nlevels=4, firstLevel=0, scaleFactor=1.3)
-    # encontramos los keypoints con ORB
-    kp = orb.detect(img)
-    # sacamos los descriptores y los key point de la imagen y los guardamos en la lista
-    kp, des = orb.compute(img, kp)
-
-    #recorremos los key point con sus atributos y los guardamos en el array con los descriptores(tupla)
-    kps_ = kps(kp,des,kps_,img)
-
-    return kps_, des
-
-def kps(kp,des,kps_, img):
+def kps(kp,img):
     # recorremos los key point con sus atributos y los guardamos en el array
+    kps =[]
     for i, key in enumerate(kp):
         x, y = key.pt
-        centroX = img.shape[0] / 2
-        centroY = img.shape[1] / 2
+        centroX = img.shape[1] / 2
+        centroY = img.shape[0] / 2
         vectorX = centroX - x
         vectorY = centroY - y
         vector = [vectorX, vectorY]
-        print(centroY - vectorY)
-        print(centroX - vectorX)
-        if vectorY==0:
+        if vectorX==0:
             anguloVec = 0
         else:
             anguloVec= np.arctan((vectorY) / (vectorX))
-        print(anguloVec)
-        modulo = np.sqrt(np.power((centroX - vectorX), 2) + np.power((centroY - vectorY), 2))
+        #print(anguloVec)
+        modulo = np.sqrt(np.power((centroX - x), 2) + np.power((centroY - y), 2))
         vectorPolar=[modulo,anguloVec]
 
-        k = (x, y, vector, vectorPolar, key.size, key.angle, key.response, key.octave, key.class_id, des[i])
-
-        kps_.append(k)
-
-    # dibujamos los keypoint en la imagen
-    img2 = cv2.drawKeypoints(img, kp, None, color=(0, 255, 0), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    plt.imshow(img2), plt.show()
-
-    return kps_
-
-
-#tablaVotacion(tabla, kpsTrain_, kpsTest_, des_, flann, imgReadTest)
-def tablaVotacion(tabla, kpsTrain, kpsTest, desTest, flann, img, imgOrigin):
-    matches = flann.knnMatch(desTest, k=5)
-
-    for match in matches:
-        for i in match:
-            #k = (x, y, vector, vectorPolar, key.size, key.angle, key.response, key.octave, key.class_id, des[i])
-            kpTestSize =kpsTest[i.queryIdx][4]
-            kpTrainSize = kpsTrain[i.trainIdx][4]
-            kpTestAngle = kpsTest[i.queryIdx][5]
-            kpTrainDistance = kpsTrain[i.trainIdx][3][0]#modulo
-            kpTrainVectorAngle = kpsTrain[i.trainIdx][3][1]
-            kpTrainAngle = kpsTrain[i.trainIdx][5]
-            kpTestDistance = kpsTest[i.queryIdx][3][0]
-            scala = kpTestSize / kpTrainSize
-
-            angulo = kpTrainVectorAngle + kpTrainAngle - kpTestAngle
-            modulo = (kpTestSize * kpTrainDistance) / kpTrainSize
-            vectorX = modulo * np.cos(angulo)
-            vectorY = modulo * np.sin(angulo)
-
-
-            kpx = int(np.divide(kpsTest[i.queryIdx][0] + vectorX, 1))
-            kpy = int(np.divide(kpsTest[i.queryIdx][1] + vectorY, 1))
-
-            if (kpx > 0 and kpy > 0) and (kpx < tabla.shape[1] and kpy < tabla.shape[0]):
-                tabla[kpy, kpx] += 1
-
-    #reagrupamos tabla
-    tabla = cv2.resize(tabla, None, fx=10, fy=10, interpolation=cv2.INTER_NEAREST)
-    max_index = np.unravel_index(tabla.argmax(), tabla.shape)
-    position = (max_index[0], max_index[1])
-    print(position)
-    cv2.circle(img, position, np.uint8(img.shape[0] / 20), (0, 0, 255), thickness=2)
-    nombre = imgOrigin.split('.')
-    cv2.imshow(nombre[0], img)
-    cv2.waitKey(0)
-
-
-'''def vectors(kp0,kpI):
-    angulo=kp0[2][1]+kp0[4]-kpI[4]
-    modulo=(kpI[3]/kp0[3])*kp0[2][0]
-    puntoX=modulo*np.cos(angulo)
-    puntoY=modulo*np.sin(angulo)
-    puntoVotacion=[kpI[0]+puntoX,kpI[1]+puntoY]
-    return puntoVotacion'''
-
-
+        k = (key.class_id,vectorPolar,vector,key.pt,key.size, key.angle, key.response, key.octave)
+        kps.append(k)
+    return kps
 def main():
-    lecturaImg()
+    lecturaImg("test")
+
+
 
 
 if __name__ == "__main__":
     main()
+
+
--- deteccion_video.py
+++ deteccion_video.py
@@ -1,6 +1,8 @@
 import os
 import cv2
 import detección_haar
+import deteccion_orb
+
 def lecturaVideo(path_name):
     cap=cv2.VideoCapture(path_name)
     while True:
@@ -17,13 +19,27 @@ def lecturaVideo(path_name):
                     cv2.destroyAllWindows()
 
 
-
-
+def lecturaVideo2(path_name):
+    cap=cv2.VideoCapture(path_name)
+    vector_votacion=[]
+    flann=deteccion_orb.createBase(vector_votacion)
+    while True:
+        if cap.grab():
+            flag,frame=cap.retrieve()
+            if not flag:
+                break
+            else:
+                deteccion_orb.detectImage(frame,flann,vector_votacion)
+                cv2.imshow("CarVid",frame)
+                key = cv2.waitKey()
+                #la ventana del imagen se cierra hasta que llega un ESC
+                if key == 27:
+                    cv2.destroyAllWindows()
 
 
 def main():
-    lecturaVideo("videos/video2.wmv")
-
+    #lecturaVideo("videos/video2.wmv")
+    lecturaVideo2("videos/video1.wmv")
 
 if __name__ == "__main__":
     main()
